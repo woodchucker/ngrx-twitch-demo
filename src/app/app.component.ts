6dee42e8bce2c1205077b2b1077767f67e513d84
woodchucker <ditrafabri@gmail.com>
2021-07-27 03:21:05 +0200

    ngrx tutorial code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,42 @@
+import { AppState } from './app.module';
+import { getItems } from './store/items.selector';
+import { Item } from './model/item';
+import { addItem, deleteItem } from './store/items.actions';
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'fdt-root',
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    <router-outlet></router-outlet>
+  <form #f="ngForm" (submit)="addItemHandler(f.value)">
+    <input type="text" name="name" [ngModel]>
+  </form>
+  <li *ngFor="let item of (items$ | async)">
+    {{item.name}}
+    <ng-container *ngIf="item.id">
+      <button (click)="deleteItemHandler(item.id)">Delete</button>
+    </ng-container>
+
+  </li>
+  <!-- <button (click)="addItemHandler()">Add</button> -->
   `,
   styles: []
 })
 export class AppComponent {
-  title = 'ngrx-twitch-demo';
+  items$: Observable<Item[]> = this.store.select(getItems);
+  items: Item[] = [];
+
+  constructor(private store: Store<AppState>) { }
+
+  addItemHandler(item: Omit<Item, 'id'>): void {
+    const formData = { id: Date.now(), ...item};
+    this.store.dispatch(addItem({ item: formData}));
+  }
+
+  deleteItemHandler(id: number): void {
+    this.store.dispatch(deleteItem({ id}));
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,17 @@
+import { Item } from './model/item';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store'
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'
+import { itemsReducer } from './store/items.reducers';
+
+export interface AppState {
+  items: Item[];
+  auth: { token: string, role: string};
+}
 
 @NgModule({
   declarations: [
@@ -10,7 +19,24 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    StoreModule.forRoot(
+      {
+        // oggetti modificabili dai reducers che ascoltano le actions aggiornano lo store
+        // le ingo saranno recuperabili con dei selector
+        // side effects simile ai reducer ascolta le action aggiorna il server
+        // reducer inizializza e muta lo store
+        items: itemsReducer,
+        auth: () => [{
+          token: 'abc123',
+          role: 'admin'
+        }]
+      }
+    ),
+    StoreDevtoolsModule.instrument({
+      maxAge:25
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
